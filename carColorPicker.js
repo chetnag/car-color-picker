b8cfec76124dba399bdff61375ca4222bcf6dec1
Chetna Ganatra <46368524+chetnag@users.noreply.github.com>
2022-03-05 19:01:20 +0530

    Add files via upload

---
+++ carColorPicker.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+const BASE_IMAGE_URL = 'https://sfdc-demo.s3-us-west-1.amazonaws.com/ecars';
+const COLORS = ['red','green','black','blue','white'];
+export default class CarColorPicker extends LightningElement {
+    colors = COLORS
+    selectedColor = this.colors[0]
+    get selectedImage(){
+        return `${BASE_IMAGE_URL}/car_${this.selectedColor}.jpg`
+    }
+
+    //dataset is the API given by HTML to fetch data-colors attribute
+    selectedHandler(event){
+        this.selectedColor = event.target.dataset.color
+    }
+}
